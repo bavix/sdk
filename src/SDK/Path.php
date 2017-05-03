15dc57ccc9519120931a4fe54beb95dea2b3bf27
REZ1DENT3 <maksim.babichev95@gmail.com>
2017-05-03 13:21:23 +0300

    add Path::slash

---
+++ src/SDK/Path.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bavix\SDK;
+
+class Path
+{
+
+    /**
+     * @param string $data
+     *
+     * @return string
+     */
+    public static function slash($data)
+    {
+        return rtrim($data, '\\/') . DIRECTORY_SEPARATOR;
+    }
+
+}
